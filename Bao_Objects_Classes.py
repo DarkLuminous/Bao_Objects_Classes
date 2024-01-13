9fd25cac1e8a30832868a8ff3ef2b9b4c97af43b
DarkLuminous <144906696+DarkLuminous@users.noreply.github.com>
2024-01-13 19:53:25 +0800

    Add files via upload

---
+++ Bao_Objects_Classes.py
@@ -0,0 +1,29 @@
+import os
+import math
+
+while True:
+    class circle:
+        def __init__(self, rad):
+            self.rad= rad
+    
+        def area(self):
+            Cir_area = math.pi * self.rad ** 2
+            print(f"\nArea = {Cir_area:.2f}")
+        
+        def parimeter(self):
+            Cir_area = 2 * math.pi * self.rad
+            print(f"Parimeter = {Cir_area:.2f}")
+            
+
+    rad = float(input("[0] = End the Program \nEnter Radius: "))
+    
+    if rad == float(0):
+       exit()
+
+    circle = circle(rad)
+        
+
+    circle.area()
+    circle.parimeter()
+    input("\n\n[Press Enter] To Continue . . .")
+    os.system('cls')
